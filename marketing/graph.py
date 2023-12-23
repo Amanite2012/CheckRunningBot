e69c736ee1a6123123493b242584a939760fb7c8
Amanite <antoine.petit@epita.fr>
2023-12-23 18:52:48 +0100

    generate first version of graph

--- marketing/graph.py
+++ marketing/graph.py
@@ -2,9 +2,12 @@
 File used to generate graphs of the imported data
 Will be used to generate files.
 """
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import datetime as dt
 
-def build_list_rain(list_tuple):
+def build_list_thermos(list_tuple):
     """
     Input: list of tuple (time, rain)
     Output: list of rain
@@ -19,7 +22,14 @@ def build_list_time(list_tuple):
     return [time for (time, rain) in list_tuple]
 
 def build_rain_graph(list_tuple):
-    list_rain = build_list_rain(list_tuple)
+    list_thermos = build_list_thermos(list_tuple)
     list_time = build_list_time(list_tuple)
+    plt.figure(figsize=(10, 6))
+    plt.plot(list_time, list_thermos)
+    plt.xlabel("Date")
+    plt.ylabel("Temperature (°C)")
+    plt.title("Temperature in Paris over the past week")
+    plt.grid(True)
+    plt.show()
     return 0
 
