2e9023ecbcefb1ffd97a6832139390d30cdebce8
Amanite <antoine.petit@epita.fr>
2023-12-24 17:57:20 +0100

    more readable graph

--- marketing/graph.py
+++ marketing/graph.py
@@ -6,29 +6,36 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import datetime as dt
+import re
 
 def build_list_thermos(list_tuple):
     """
     Input: list of tuple (time, rain)
     Output: list of rain
     """
-    return [rain for (time, rain) in list_tuple]
+    return [temp for (time, temp) in list_tuple]
 
 def build_list_time(list_tuple):
     """
     Input: list of tuple (time, rain)
     Output: list of time
     """
-    return [time for (time, rain) in list_tuple]
+    ret = [time for (time, temp) in list_tuple]
+    # Makes the date more readable
+    for i in ret:
+        ret[ret.index(i)] = i[5:13] + "h"
+    return ret
 
 def build_rain_graph(list_tuple):
     list_thermos = build_list_thermos(list_tuple)
     list_time = build_list_time(list_tuple)
-    plt.figure(figsize=(10, 6))
     plt.plot(list_time, list_thermos)
+
+    # Put the X axis caption vertially
+    plt.xticks(rotation=90, horizontalalignment='center', fontsize=4)
     plt.xlabel("Date")
     plt.ylabel("Temperature (°C)")
-    plt.title("Temperature in Paris over the past week")
+    plt.title("Temperature in Paris over the next week")
     plt.grid(True)
     plt.show()
     return 0
