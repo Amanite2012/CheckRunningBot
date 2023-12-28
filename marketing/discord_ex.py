e240d990e08389aa04801c1e99b2e2e73d9f639b
Amanite <antoine.petit@epita.fr>
2023-12-28 18:57:48 +0100

    Commit before graille

--- marketing/discord_ex.py
+++ marketing/discord_ex.py
@@ -8,24 +8,21 @@ import os, dotenv
 
 # Objective calculate optimal conditions to go for a run
 
+# Input: list of tuples (time, rain, temperature, humidity)
+# Output dictionnary for recommandations
+def build_reco(string):
+    ret = {"01 h": None, "04h": None, "07h": None, "10h": None, "13h": None, "16h": None, "19h": None, "22h": None}
+    return ret
+
+
 def send_message():
     dotenv.load_dotenv()
     webhook_url = os.getenv("WEBHOOK_DISCORD")
-    print(webhook_url)
+    message = {"ayoh": ':green_circle:', "test": 3}
+    msg = str(message)
     data = {
-    "content": "Bonjour et bienvuenue pour les prévisions de la semaine TODO.\nVoici les prévisions:\n**Lundi**: TODO\n**Mardi**: TODO",
-    "embeds": [
-      {
-        "title": "Méthodes de suggestion",
-        "description": "Il s'avère que la température pour maximiser les performances est de 9 de degrés pour les femmes et de 6 degrés pour les hommes (https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0037407#pone-0037407-g004). Cette température a tendance a diminuer avec l'augmentation des capacités physiques.\n\nQuant à la pollution, nous nous sommes intéressés à l'impact sur les performances immédiates. Il s'avère que courir avec une pollution (AQI) de + de 100 équivaut à une augmentation au temps de +4%. (https://www.lung.org/blog/running-outside-air-pollution)\nC'est pourquoi nous vous déconseillerons toujours les périodes de fortes pollution.",
-        "author": {
-          "name": "Par Amanite"
-        }
-      }
-    ],
-    "username": "Running bot",
-    "attachments": []
-  }
+        "content": msg
+    }
 
     headers = {
         "Content-Type": "application/json"
